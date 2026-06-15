1c43b6bb35736364a0b3a7056af4afd017d6039f
twappymax-fr <aengwocaleb@gmail.com>
2026-06-15 11:00:12 +0300

    Changed debug=True to debug

--- DjangoProject1/watchu/settings.py
+++ DjangoProject1/watchu/settings.py
@@ -23,8 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv("DEBUG", "False").lower() == "true"
-
+# DEBUG = os.getenv("DEBUG", "False").lower() == "true"
+DEBUG = True
 print(f"this is the current debug value: {DEBUG}")
 print(f"from the env file: {os.getenv('DEBUG')}")
 
